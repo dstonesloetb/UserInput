bf847318fe995868edb7a1a24e50d5075cd3715b
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-09-23 10:25:13 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+// Student Name :      D.Stones
+// Student Id Number : COM1234
+// Date : 
+// Purpose : A Sample skeleton program
+
+class Main {
+  public static void main(String[] args) {
+    //Declare and initialise the variable
+    String name = "";
+
+    //Input the user's name
+		System.out.print("Enter your name: ");
+		name = EasyIn.getString();
+
+    //Process the input
+    System.out.println("Hello " + name);
+		
+    //Output
+    System.out.println("Have a nice day !");
+  } //end main  
+} //end class
