008f02f33f479400c0e3c332b9d47f1b245ab407
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-09-26 11:21:19 +0100

    Create Example1.java

---
+++ Example1.java
@@ -0,0 +1,18 @@
+package com.mycompany.introexamples;
+
+/*
+ * Purpose: Demo outputting hello world 
+ * @author Darren Stones
+ * Date: 19 Sept 2024 
+ */
+
+//Class declaration
+     
+        
+public class Example1 {
+
+    public static void main(String[] args) {
+        //Output 'Hello World!' to the console window
+        System.out.println("Hello World!");
+    } // end main 
+} //End Class 
